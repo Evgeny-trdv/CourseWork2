b0061f6ef70669ed8ba77ce765d0a9b9d29f13ab
evgen <evgeny15q@gmail.com>
2025-01-27 14:20:43 +0700

    Start course work 2

---
+++ src/main/java/pro/sky/course_two_questions/CourseTwoQuestionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course_two_questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseTwoQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseTwoQuestionsApplication.class, args);
+	}
+
+}
